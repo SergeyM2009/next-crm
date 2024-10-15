ebaad9ad7f0344792eb135920b858bcf1d47749a
Serg <weramor73@gmail.com>
2024-10-15 21:13:48 +0300

    гламур

--- src/friends.js
+++ src/friends.js
@@ -29,7 +29,7 @@ const  makeTime = ()=>  currentTimeMoscow = new Date().toLocaleString('ru-RU', {
 // const serverAdress = "https://grizzly-boiled-broccoli.glitch.me/posts";
 // const serverAdress = "https://market.teplo-best.ru/posts";
 // const serverAdress = "http://localhost:5000/posts";
-const serverAddress = "https://raw.githubusercontent.com/SergeyM2009/next-crm/master/db.json/posts";
+const serverAddress = "https://github.com/SergeyM2009/next-crm/blob/master/db.json/posts";
 
 
 export {renderPriduct, renderState,badgeColor, makeDate, makeTime,currentDate,currentTimeMoscow, serverAdress}
